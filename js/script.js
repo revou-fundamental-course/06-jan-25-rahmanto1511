f12c8e8499955383cf0a8112e9b08079e06044bb
rahmanto1511 <rahmanto1511@gmail.com>
2025-01-15 18:24:37 +0700

    rev2

--- js/script.js
+++ js/script.js
@@ -9,7 +9,28 @@ function sapa() {
     }
 }
 
-sapa();
+//sapa();
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusBanner(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var imgList = document.getElementsByClassName("banner");
+    console.log(imgList);
+    if (n > imgList.length) slideIndex = 1;
+    else if (n < 1) slideIndex = imgList.length;
+
+    for(i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = "none";
+    }
+
+    imgList[slideIndex-1].style.display = "block";
+}
 
 function tampilkanform(nama, lahir, kelamin, pesan) {
     document.getElementById("time").innerHTML = "<b>Time : </b>" + Date();
